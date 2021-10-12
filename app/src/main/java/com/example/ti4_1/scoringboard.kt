a16929224aba18d144933a0729a9c95f7d4bc7f5
konsent <konsent@naver.com>
2021-10-13 00:16:12 +0900

    #2 임시주석

--- app/src/main/java/com/example/ti4_1/scoringboard.kt
+++ app/src/main/java/com/example/ti4_1/scoringboard.kt
@@ -147,7 +147,7 @@ class scoringboard : AppCompatActivity(){
                       p1_sum_mr.text = sb_spinner_mr_p1.toString()
                 }
             }
-            //여기를 봐바라 바바라2
+            //여기를 봐바라 바바라3
             sb_p1_sum.setText(secretsum)
 
 
